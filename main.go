77ab859bc34ceb3b263e471cc347d6cdac9056e2
Tim <timothee.bull@gmail.com>
2026-06-17 12:26:35 +0200

    feat(pages): serve "/" path

--- main.go
+++ main.go
@@ -7,11 +7,25 @@ import (
 
 func main() {
 	const port = "8080"
-	mux := http.NewServeMux()
+	mux := initMux()
 	server := http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
 	log.Fatal(server.ListenAndServe())
+}
+
+func initMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	registerRoutes(mux)
+	return mux
+}
+
+func registerRoutes(mux *http.ServeMux) {
+	mux.Handle("/{$}", root())
+}
+
+func root() http.Handler {
+	return http.FileServer(http.Dir("."))
 }
